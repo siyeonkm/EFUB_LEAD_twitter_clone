fb82df967bd1548b86246743c87c09e80c6fe7b7
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-05-03 17:27:43 +0900

    feat(UserController) : user 관련 api 코드 작성

--- src/main/java/efub/lead/twitter/controller/UserController.java
+++ src/main/java/efub/lead/twitter/controller/UserController.java
@@ -1,4 +1,42 @@
 package efub.lead.twitter.controller;
 
+import efub.lead.twitter.controller.dto.FollowResponseDTO;
+import efub.lead.twitter.controller.dto.TweetResponseDTO;
+import efub.lead.twitter.controller.dto.UserResponseDTO;
+import efub.lead.twitter.service.FollowService;
+import efub.lead.twitter.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+    private final FollowService followService;
+
+    @GetMapping("/{user_id}")
+    public UserResponseDTO searchUserById(@PathVariable Long user_id) {
+        return new UserResponseDTO();
+    }
+
+    @PatchMapping("/{user_id}")
+    public UserResponseDTO updateUserById(@PathVariable Long user_id) {
+        return new UserResponseDTO();
+    }
+
+    @GetMapping("/{user_id}/mytweets")
+    public TweetResponseDTO searchTweetsById(@PathVariable Long user_id) {
+        return new TweetResponseDTO();
+    }
+
+    @GetMapping("/{user_id}/followee")
+    public FollowResponseDTO searchFolloweesById(@PathVariable Long user_id) {
+        return new FollowResponseDTO();
+    }
+
+    @GetMapping("/{user_id}/follower")
+    public FollowResponseDTO searchFollowersById(@PathVariable Long user_id) {
+        return new FollowResponseDTO();
+    }
 }
--- src/main/java/efub/lead/twitter/service/UserService.java
+++ src/main/java/efub/lead/twitter/service/UserService.java
@@ -1,4 +1,11 @@
 package efub.lead.twitter.service;
 
+import efub.lead.twitter.controller.dto.FollowResponseDTO;
+import efub.lead.twitter.controller.dto.TweetResponseDTO;
+import efub.lead.twitter.controller.dto.UserResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
 public class UserService {
+
 }
