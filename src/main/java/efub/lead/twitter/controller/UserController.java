9a8746edec6c354f65291d6244cde384dd9296f7
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-05-04 17:41:51 +0900

    feat(User) : user register api 생성
    테스트 편리성을 위해서 유저 추가 api를 따로 만드는중

--- src/main/java/efub/lead/twitter/controller/UserController.java
+++ src/main/java/efub/lead/twitter/controller/UserController.java
@@ -1,4 +1,21 @@
 package efub.lead.twitter.controller;
 
+import efub.lead.twitter.controller.dto.UserRequestDTO;
+import efub.lead.twitter.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/registration")
+    public Long createUser(@RequestBody UserRequestDTO userDTO) {
+        return userService.createUser(userDTO);
+    }
+
 }
--- src/main/java/efub/lead/twitter/service/UserService.java
+++ src/main/java/efub/lead/twitter/service/UserService.java
@@ -1,4 +1,18 @@
 package efub.lead.twitter.service;
 
+import efub.lead.twitter.controller.dto.UserRequestDTO;
+import efub.lead.twitter.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Long createUser(UserRequestDTO userDTO) {
+        
+    }
+
 }
