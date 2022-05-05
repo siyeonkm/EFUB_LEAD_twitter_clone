a348bc35180bc9853a025075487d0809f8115279
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2022-05-05 21:18:15 +0900

    fix(close #3) : 현 브랜치에 맞지 않는 api 삭제

--- src/main/java/efub/lead/twitter/controller/UserController.java
+++ src/main/java/efub/lead/twitter/controller/UserController.java
@@ -18,7 +18,6 @@ public class UserController {
 
     private final UserService userService;
     private final FollowService followService;
-    private final TweetService tweetService;
 
     @GetMapping("/registration")
     public UserResponseDTO createUser(@RequestBody UserRequestDTO userDTO) {
@@ -26,28 +25,18 @@ public class UserController {
     }
 
     @GetMapping("/{user_id}")
-    public UserResponseDTO findUserById(@RequestParam Long user_id) {
+    public UserResponseDTO findUserById(@PathVariable Long user_id) {
         return userService.findUserById(user_id);
     }
 
-    @PatchMapping("/{user_id}")
-    public String updateUserById(@RequestParam Long user_id) {
-        return "modified";
-    }
-
-    @PatchMapping("/{user_id}/mytweets")
-    public List<TweetResponseDTO> findTweetsById(@RequestParam Long user_id) {
-        return tweetService.findTweetsById(user_id);
-    }
-
     @GetMapping("/{user_id}/followee")
-    public List<FollowResponseDTO> findFolloweeById(@RequestParam Long user_id) {
+    public List<FollowResponseDTO> findFolloweeById(@PathVariable Long user_id) {
         List<Long> followees = followService.findFolloweesById(user_id);
         return userService.findUsersByFolloweeId(followees);
     }
 
     @GetMapping("/{user_id}/follower")
-    public List<FollowResponseDTO> findFollowerById(@RequestParam Long user_id) {
+    public List<FollowResponseDTO> findFollowerById(@PathVariable Long user_id) {
         List<Long> followers = followService.findFollowersById(user_id);
         return userService.findUsersByFollowerId(followers);
     }
